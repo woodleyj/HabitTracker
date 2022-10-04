473218de69abf42f616957e61861ef21c3c6404f
Jonathan Woodley <woodley.jonathan@gmail.com>
2022-10-04 11:48:10 +0200

    Reorganized and changed setup/teardown of test class in test_project.py

--- resources/test_project.py
+++ resources/test_project.py
@@ -2,23 +2,22 @@ import sqlite3
 
 from database import DBConn
 import os
-import time
 
 
 class TestDB:
 
-    def setup_method(self):
+    def setup_class(self):
         self.db = DBConn(name="test.db")
         self.db.starter_habits()
 
     def test_add_record(self):
         self.db.add_record("Test Habit", "Test Desc", "Daily")
 
-    # def test_get_interval(self):
-    #     self.db.get_interval("Test Habit")
+    def test_get_interval(self):
+        self.db.get_interval("Test Habit")
 
-    # def test_complete_task(self):
-    #     self.db.complete_task("Test Habit")
+    def test_complete_task(self):
+        self.db.complete_task("Test Habit")
 
     def test_fetch_habit_history(self):
         self.db.fetch_habit_history("Test Habit")
@@ -30,8 +29,8 @@ class TestDB:
         self.db.update_record("Test Habit", "name", "Updated Test Habit")
 
     def test_update_streak(self):
-        self.db.update_streak("Updated Test Habit")
-        self.db.update_streak("Update Test Habit", reset_streak=True)
+        self.db.update_streak("Drink Water")
+        self.db.update_streak("Drink Water", reset_streak=True)
 
     def test_delete_record(self):
         self.db.delete_record("Updated Test Habit")
@@ -39,6 +38,6 @@ class TestDB:
     def test_display_habit_name(self):
         self.db.display_habit_names()
 
-    def teardown_method(self):
-        self.db.conn.close()
+    def teardown_class(self):
+        self.db.conn.close()  # Produces sqlite3 Error, but if I don't include this line then db doesn't delete
         os.remove("test.db")
